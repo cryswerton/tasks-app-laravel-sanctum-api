b140542549987da74807d571af4999015513a105
cryswerton <cryswertons@gmail.com>
2023-04-22 17:32:45 +0100

    StoreUserRequest added.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TasksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post("/login", [AuthController::class, 'login']);
 Route::post("/register", [AuthController::class, 'register']);
 Route::post("/logout", [AuthController::class, 'logout']);
+
+Route::resource("/tasks", TasksController::class);
 
 
